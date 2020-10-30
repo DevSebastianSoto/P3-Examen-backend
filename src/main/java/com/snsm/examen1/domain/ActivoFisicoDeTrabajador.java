4a5fbcf6013cf237c2e813032512d780b2eb22d2
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2020-10-30 10:43:32 -0600

    removed unused import

--- src/main/java/com/snsm/examen1/domain/ActivoFisicoDeTrabajador.java
+++ src/main/java/com/snsm/examen1/domain/ActivoFisicoDeTrabajador.java
@@ -3,7 +3,6 @@ package com.snsm.examen1.domain;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
