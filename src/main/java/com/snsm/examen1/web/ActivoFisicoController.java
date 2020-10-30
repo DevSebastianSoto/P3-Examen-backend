538bb8b7bd0fa69dfe37d4be73a597c305b5e73f
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2020-10-29 22:08:48 -0600

    cors

--- src/main/java/com/snsm/examen1/web/ActivoFisicoController.java
+++ src/main/java/com/snsm/examen1/web/ActivoFisicoController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * ActivoFisicoController
  */
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api/activos-fisicos")
 public class ActivoFisicoController {
--- src/main/java/com/snsm/examen1/web/ActivoFisicoDeTrabajadorController.java
+++ src/main/java/com/snsm/examen1/web/ActivoFisicoDeTrabajadorController.java
@@ -6,6 +6,7 @@ import com.snsm.examen1.service.ActivoFisicoDeTrabajadorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * ActivoFisicoDeTrabajadorController
  */
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("api/activo-trabajador")
 public class ActivoFisicoDeTrabajadorController {
